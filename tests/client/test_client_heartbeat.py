4b49f7b12e23cca11a96326cb1b77f99fa0e1052
rafa-be <raphael@noisycamp.com>
2026-08-24 17:41:18 +0200

    fixup! Add a unit-test checking the client does not hang during a long OSS operation.
    Signed-off-by: rafa-be <raphael@noisycamp.com>

--- tests/client/test_client_heartbeat.py
+++ tests/client/test_client_heartbeat.py
@@ -4,9 +4,9 @@ from scaler import Client, SchedulerClusterCombo
 from scaler.utility.logging.utility import setup_logger
 from tests.utility.utility import logging_test_name
 
-CLIENT_TIMEOUT_SECONDS = 1
+SCHEDULER_CLIENT_TIMEOUT_SECONDS = 2
 
-LARGE_RESULT_SIZE_BYTES = 500_000_000
+LARGE_RESULT_SIZE_BYTES = 750_000_000
 
 
 def noop(value: int) -> int:
@@ -22,7 +22,7 @@ class TestClientHeartbeat(unittest.TestCase):
         setup_logger()
         logging_test_name(self)
         self.combo = SchedulerClusterCombo(
-            n_workers=1, event_loop="builtin", client_timeout_seconds=CLIENT_TIMEOUT_SECONDS
+            n_workers=1, event_loop="builtin", client_timeout_seconds=SCHEDULER_CLIENT_TIMEOUT_SECONDS
         )
         self.address = self.combo.get_address()
 
@@ -32,16 +32,16 @@ class TestClientHeartbeat(unittest.TestCase):
     def test_client_non_blocking(self):
         """Receiving a large task result must not prevent the client agent's event loop to send heart-beats."""
 
-        FUTURE_RESULT_TIMEOUT = CLIENT_TIMEOUT_SECONDS * 5
+        FUTURE_RESULT_TIMEOUT_SECONDS = 60
 
-        with Client(self.address, timeout_seconds=CLIENT_TIMEOUT_SECONDS) as client:
+        with Client(self.address, heartbeat_interval_seconds=1) as client:
             future = client.submit(large_result, LARGE_RESULT_SIZE_BYTES)
 
-            self.assertEqual(len(future.result(timeout=FUTURE_RESULT_TIMEOUT)), LARGE_RESULT_SIZE_BYTES)
+            self.assertEqual(len(future.result(timeout=FUTURE_RESULT_TIMEOUT_SECONDS)), LARGE_RESULT_SIZE_BYTES)
 
             # the client must still be connected to the scheduler, i.e. it kept exchanging heartbeats while fetching the
             # result object.
-            self.assertEqual(client.submit(noop, 1).result(), 1)
+            self.assertEqual(client.submit(noop, 1).result(timeout=FUTURE_RESULT_TIMEOUT_SECONDS), 1)
 
 
 if __name__ == "__main__":
